1b34fab40ae6b8e97783d38c535ee127286cccf8
Jeremy Duncan <jduncan46@my.devry.edu>
2023-10-22 22:23:41 -0400

    added host

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module CatTinderBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.hosts << "cattinder.jeremyduncan.synology.me"
 
     # Configuration for the application, engines, and railties goes here.
     #
