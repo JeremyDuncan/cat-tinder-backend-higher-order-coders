3950173ab9727e50d383c8ad4c7954c02a406a29
JeremyDuncan <jduncan46@my.devry.edu>
2024-02-15 13:22:12 -0500

    removed delete options

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -28,24 +28,58 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
   end
 
+  # def update
+  #   @cat = Cat.find(params[:id])
+  #   if @cat.update(cat_params)
+  #     redirect_to @cat, notice: 'Cat profile was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
   def update
     @cat = Cat.find(params[:id])
-    if @cat.update(cat_params)
+    puts ""
+    puts "===================="
+    puts "params[:id] => #{params[:id]}"
+    puts "@cat => #{@cat}"
+
+    # Filter out empty image strings
+    filtered_params = cat_params.deep_dup
+    puts ""
+    puts "filtered_params INITIAL=> #{filtered_params}"
+
+    filtered_params[:cat_images_attributes].each do |key, attributes|
+      puts ""
+      puts "attributes[:image] => #{attributes[:image]}"
+      # pp attributes[:image].inspect
+
+      if attributes[:image].is_a?(Array)
+        attributes[:image].reject!(&:blank?)
+      end
+    end
+    puts ""
+    puts "filtered_params AFTER=> #{filtered_params}"
+
+    if @cat.update(filtered_params)
       redirect_to @cat, notice: 'Cat profile was successfully updated.'
     else
       render :edit
     end
   end
 
-  def destroy
-    @cat = Cat.find_by(id: params[:id])
-    if @cat
-      @cat.destroy
-      redirect_to cats_path, notice: 'Cat was successfully deleted.'
-    else
-      redirect_to cats_path, alert: 'Cat not found.'
-    end
-  end
+
+
+
+
+  # def destroy
+  #   @cat = Cat.find_by(id: params[:id])
+  #   if @cat
+  #     @cat.destroy
+  #     redirect_to cats_path, notice: 'Cat was successfully deleted.'
+  #   else
+  #     redirect_to cats_path, alert: 'Cat not found.'
+  #   end
+  # end
 
   def delete
     @cat = Cat.find(params[:id])
@@ -59,4 +93,16 @@ class CatsController < ApplicationController
     params.require(:cat).permit(:name, :age, :enjoys,
                                 cat_images_attributes: [:id, :image, :_destroy])
   end
+
+
+  # def cat_params
+  #   params.require(:cat).permit(:name, :age, :enjoys,
+  #                               cat_images_attributes: [:id, :image, :_destroy], images: [])
+  # end
+  # def cat_params
+  #   params.require(:cat).permit(:name, :age, :enjoys,
+  #                               cat_images_attributes: [:id, { image: [] }, :_destroy])
+  # end
+
+
 end
